cea3e93de338ad567f2c5bc4b2575a938de9d832
MakakArt <koro.havikov@gmail.com>
2021-03-27 19:01:19 +0300

    GOSHA PIDARAD

---
+++ day4/homework_Arthur_script.php
@@ -0,0 +1,47 @@
+<?
+
+$link = "localhost";
+$login = "mysql";
+$pass = "mysql";
+$db = "phplearn";
+
+$sql = new mysqli($link, $login, $pass, $db);
+
+if ($_GET['createdb'] == "create")
+{
+    $sql->query("CREATE TABLE IF NOT EXISTS `visitorss` (
+        `id` int(11) NOT NULL primary key AUTO_INCREMENT,
+        `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+        `surname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+        `comment` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci");
+        header("Location: /day4/homework_Arthur.php");
+}
+
+if ($_GET['delitedbdb'] == "create")
+{
+    $sql->query("DROP TABLE `visitorss`");
+    header("Location: /day4/homework_Arthur.php");
+}
+
+if ($_GET["cleandb"] == "create")
+{
+    $sql->query("TRUNCATE TABLE `visitorss`");
+    header("Location: /day4/homework_Arthur.php");
+}
+
+if ($_GET['name'] && $_GET['surname'] && $_GET['comment'] && $_GET['action'] == "add")
+{
+    $sql->query("INSERT INTO `visitorss` (`name`, `surname`, `comment`) VALUES ('" .$_GET['name']. "', '".$_GET['surname']."', '".$_GET['comment']."')");
+    header("Location: /day4/homework_Arthur.php");
+}
+else
+{
+    header("Location: /day4/homework_Arthur.php?error=1");
+}
+
+if ($_GET['id'])
+{
+    $sql->query("DELETE FROM `visitorsss` WHERE `id`=".$_GET['id']);
+    header("Location: /day4/homework_Arthur.php");
+}
