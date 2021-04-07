e935a8c0e0bbc54cef3565a249278959391db38d
артур <koro.havikov@gmail.com>
2021-04-07 20:17:27 +0300

    filtr

--- day5/homework_Arthur_d5.php
+++ day5/homework_Arthur_d5.php
@@ -29,28 +29,48 @@
 <label  for=""><input name="fsurname" type="text" placeholder = "Введите фамилию"></label><br>
 <button name="search" value="surname">Отфильтровать по фамилии</button>
 </form>
-<a href="./homework_Arthur.php">Очистить фильтрацию</a>
-
+<?
+$searh_surname = $_GET['fsurname'];
+if ($searh_surname)
+echo "Фильтр: $searh_surname<br>"
+?>
+<a href="./homework_Arthur_d5.php">Очистить фильтрацию</a>
  <h2>Комментарии посетителей</h2>
 <?
+
 if ($_GET['error'])
 {
     echo "Ошибка. Заполните все поля";
 }
 
-
+if (file_exists("./json_homework.json"))
+{
 $json = file_get_contents("json_homework.json");
 $json = json_decode($json);
 
 foreach ($json as $row)
     {
-       $name = $row->NAME;
-       $surname = $row->SURNAME;
-       $comment = $row->COMMENT;
-       echo "Имя: $name <br> Фамилия: $surname <br> Комментарий: $comment<br><br> <br>";
+        if ($_GET['search'] == "surname")
+        {
+            $name = $row->NAME;
+            $surname = $row->SURNAME;
+            $comment = $row->COMMENT;
+            if ($searh_surname == $surname)
+            {
+                echo "Имя: $name <br> Фамилия: $surname <br> Комментарий: $comment<br><a href='#'>Удалить запись</a><br><a href='#'>Редактировать запись</a><br><br> <br>";
+            }
+        }
+        else
+        {
+            $name = $row->NAME;
+            $surname = $row->SURNAME;
+            $comment = $row->COMMENT;
+            echo "Имя: $name <br> Фамилия: $surname <br> Комментарий: $comment<br><a href='#'>Удалить запись</a><br><a href='#'>Редактировать запись</a><br><br> <br>";
+        }
     }
 
-
+}
+else echo 'Файла JSON не существует';
 
 ?>
 
--- day5/homework_Arthur_d5_script.php
+++ day5/homework_Arthur_d5_script.php
@@ -10,11 +10,11 @@ if ($_GET['name'] && $_GET['surname'] && $_GET['comment'] )
     $json = file_get_contents("json_homework.json");
     $json = json_decode($json);
     $json[] = array("NAME" => $name, "SURNAME" => $surname, "COMMENT" => $comment);
-    var_dump($json);
     $json = json_encode($json);
     $json_write = fopen("./json_homework.json", "w+");
     fwrite($json_write, $json.PHP_EOL);
     fclose($json_write);
+    header("Location: /day5/homework_Arthur_d5.php");
 
 }
 else if ($_GET['action'] == "add")
@@ -27,12 +27,39 @@ if ($_GET['delitedbdb'] == "create")
 {
     if (file_exists("./json_homework.json"))
     {
+        unlink("./json_homework.json");
+        header("Location: /day5/homework_Arthur_d5.php");
+        
+    }
+    else
+    {
+        header("Location: /day5/homework_Arthur_d5.php");
+    }
+}
+
+if ($_GET['createdb'] == "create")
+{
+    if (file_exists("./json_homework.json"))
+    {
         header("Location: /day5/homework_Arthur_d5.php");
     }
     else
     {
-     unlink("./json_homework.json");
-     header("Location: /day5/homework_Arthur_d5.php");
+        file_put_contents("./json_homework.json", '');
+        header("Location: /day5/homework_Arthur_d5.php");
+    }
+}
+
+if ($_GET["cleandb"] == "create")
+{
+    if (file_exists("./json_homework.json"))
+    {
+        file_put_contents("./json_homework.json", null);
+        header("Location: /day5/homework_Arthur_d5.php");
+    }
+    else
+    {
+        header("Location: /day5/homework_Arthur_d5.php");
     }
 }
 
